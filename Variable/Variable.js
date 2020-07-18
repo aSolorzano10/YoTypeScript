eb52c2b820e5deba28a961e951bccf344b01d0b7
alfredo_solorzano <asolorzanoaguilar@yahoo.com>
2020-07-18 15:57:24 -0500

    added valdiation typeof to variables primitives and wrapers

---
+++ Variable/Variable.js
@@ -0,0 +1,18 @@
+var cadena = "Hola";
+var numero = 5;
+var verdadero = true;
+var varAny;
+var arreglo = [];
+var arregloObject = {};
+var cadenaEnvltorio = new String("Adios");
+var numeroEnvoltorio = new Number(10);
+var verdaderoEnvoltorio = new Boolean(true);
+console.log("string Primitivo: ", typeof cadena);
+console.log("number primitivio: ", typeof numero);
+console.log("bolean Primitivo: ", typeof verdadero);
+console.log("variable Any:", typeof varAny);
+console.log("array []: ", typeof arreglo);
+console.log("object {}: ", typeof arregloObject);
+console.log("String Envoltorio: ", typeof cadenaEnvltorio);
+console.log("Number Envoltorio: ", typeof numeroEnvoltorio);
+console.log("Boolean Envoltorio: ", typeof verdaderoEnvoltorio);
