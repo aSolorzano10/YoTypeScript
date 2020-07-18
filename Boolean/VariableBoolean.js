aff7605333f3c9fa7b38aa39366ccb58424f926b
alfredo_solorzano <asolorzanoaguilar@yahoo.com>
2020-07-18 13:37:38 -0500

    added initializa variable boolean

---
+++ Boolean/VariableBoolean.js
@@ -0,0 +1,4 @@
+var boolean1 = true;
+var boolean2 = new Boolean(false);
+console.log("boolean 1: ", boolean1);
+console.log("boolean 2: ", boolean2);
---
+++ Boolean/VariableBoolean.ts
@@ -0,0 +1,5 @@
+var boolean1: boolean = true;
+var boolean2 = new Boolean(false);
+
+console.log("boolean 1: ", boolean1);
+console.log("boolean 2: ", boolean2);
